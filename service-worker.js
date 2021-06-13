358b1deb7b075cd39e541a4241ebcf5a0f8fbb85
amithos <alex.theutos@gmail.com>
2021-06-14 00:51:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9b9b912321e3d315fc8f540d885eee7f.js"
+  "./precache-manifest.fb925bf0f2cd8a700365cf21776e9e64.js"
 );
 
 self.addEventListener('message', (event) => {
